0983740d570f7b1628a029248957a446e3011566
Herman Morales <nitrowitty@gmail.com>
2026-01-14 18:28:39 -0600

    adding dashboard layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import AuthLayout from '@/layouts/auth-layout'
+import DashboardLayout from '@/layouts/dashboard-layout'
 import '@/styles/globals.css'
 import Login from '@/views/auth/login.tsx'
+import Home from '@/views/dashboard/home'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
@@ -9,7 +11,9 @@ createRoot(document.getElementById('root')!).render(
 		<Routes>
 			<Route path='auth' element={<AuthLayout/>}>
 				<Route index  path='login' element={<Login/>} />
-				
+			</Route>
+			<Route path='dashboard' element={<DashboardLayout/>}>
+				<Route index  path='' element={<Home/>} />
 			</Route>
 		</Routes>
   </BrowserRouter>,
